9d452cbe0897efcd8f787df6dc7332f74eef6dca
Agung Sapto Margono Dh <agungdh@live.com>
2018-11-28 13:56:18 +0700

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App() {
 			<div className="row justify-content-center">
 				<div className="col-md-auto">
 					<div className="input-group">
-						<input type="text" className="form-control" placeholder="To-Dos" />
+						<input type="text" className="form-control" placeholder="To-Do" />
 						<div className="input-group-prepend">
 							<button className="btn btn-success">
 								<i className="fa fa-check" />
